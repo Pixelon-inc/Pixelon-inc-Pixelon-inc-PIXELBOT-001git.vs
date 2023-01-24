f02f69051b334b8f8d5892e019659991d38f6a5e
Kauã Frigieri <41579326+httppixelon@users.noreply.github.com>
2023-01-23 21:17:37 -0300

    Update Automod1.js

---
+++ Commands/Automod1.js
@@ -0,0 +1,7 @@
+client.on('message', message => {   
+
+  if (message.content.includes('Sexo')) {     
+    message.delete();     
+ message.reply('Mantenha o respeito, evite palavrões neste servidor! 🤡');   
+    } 
+ });
